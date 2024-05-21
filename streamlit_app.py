ce4c398e8043650f7a8510f069b319a64548e3d0
Anuradha_Ganapathi <anuganapath@gmail.com>
2024-05-21 06:10:09 +0200

    Updated streamlit endpoint name

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import streamlit as st
 import boto3
 import json
+import nltk
+
 
 email = st.text_area("Text to be analyzed",key='email')
 
@@ -12,27 +14,35 @@ if btn1:
     st.write(email)
 
  
-endpoint = 'blazingtext-2024-05-10-13-30-15-678'
+endpoint = 'blazingtext-2024-05-18-20-34-01-612'
  
 runtime = boto3.client('sagemaker-runtime',region_name="eu-north-1")
-messages = [
-                # Spam
-                'Click on below link, provide your details and win this award' ,
-                'Best summer deal here',
-                #ham
-                'See you in the office on Friday.'
+#messages = [
+ #               # Spam
+  #              'Click on below link, provide your details and win this award' ,
+   #             'Best summer deal here',
+    #            #ham
+     #           'See you in the office on Friday.'
+#
+#]
 
-]
+messages = [email]
 
-# Set the input data
-input_data = {
-        'data': ['input_value_1', 'input_value_2']
-    }
+tokenized_message = [' '.join(nltk.word_tokenize(mesaage)) for mesaage in messages]
+payload = {"instances" : tokenized_message}
+payload = json.dumps(payload)
 
-    # Convert the input data to JSON format
-payload = json.dumps(input_data)
+# Convert the input data to JSON format
+#payload = json.dumps(input_data)
 # Send image via InvokeEndpoint API
-response = runtime.invoke_endpoint(EndpointName=endpoint, ContentType='application/json', Body=payload)
+response = runtime.invoke_endpoint(EndpointName=endpoint, ContentType='application/json', Body=payload.encode())
 
 # Unpack response
 result = json.loads(response['Body'].read().decode())
+print(result)
+
+for res in result:
+    final_value = res['label']
+
+for test in final_value:
+    print(test.replace('__label__',''))
